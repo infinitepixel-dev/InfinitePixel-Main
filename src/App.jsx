d42be32b41b055596ed9ee7b25344b2e15a7d351
Matthew Cox <mattdrummerguy@gmail.com>
2023-10-21 10:15:54 -0500

    Added basic page

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  //range of numbers to multiply by
+  let random = Math.floor(Math.random() * (100 - 1 + 1)) + 1; //random number generator
+
+  let currentYear = new Date().getFullYear(); //current year
+
+  return (
+    <>
+      <div className="App">
+        <h1>InfinitePixel!</h1>
+      </div>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + random)}>
+          Pixel Counter {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        {/* Coming Soon with a copyright symbol */}
+        <h3>© InfinitePixel {currentYear}</h3>
+      </p>
+    </>
+  );
+}
+
+export default App;
