526bb112f6dbdbf543e8ce607e365db12d9f8a41
Masheen88 <mattdrummerguy@gmail.com>
2024-03-27 14:51:45 -0500

    resolved app css path

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ let displaySplashPage = true; //set to true when deploying to production
 
 // MUST BE IMPORTED ON ALL COMPONENTS
 import DynamicComponentLoader from "./dynamicComponentLoader";
-import "./app.css";
 
 function App() {
   const [count, setCount] = useState(0);
