7cfe2753f111c50286eaebdbfa46e98d881f09c8
Masheen88 <mattdrummerguy@gmail.com>
2024-04-16 17:12:11 -0500

    Refactor page2.jsx to remove background color divs

--- src/components/pages/page2.jsx
+++ src/components/pages/page2.jsx
@@ -5,55 +5,6 @@ function HeroPage() {
   return (
     <div className="relative col-span-12 row-span-1 row-start-1 overflow-hidden">
       {/*NOTE Background Colors */}
-      <div
-        className="absolute bg-color1"
-        style={{
-          width: "50%",
-          height: "50%",
-          left: "0%",
-          top: "50%",
-          filter: "blur(200px)",
-          zIndex: "-1",
-          overflow: "hidden",
-        }}
-      ></div>
-      <div
-        className="absolute bg-color2"
-        style={{
-          width: "40%",
-          height: "60%",
-          left: "20%",
-          top: "15%",
-          filter: "blur(200px)",
-          zIndex: "-1",
-          overflow: "hidden",
-        }}
-      ></div>
-      <div
-        className="absolute bg-color3"
-        style={{
-          width: "30%",
-          height: "40%",
-          left: "50%",
-          top: "5%",
-          filter: "blur(200px)",
-          zIndex: "-1",
-          overflow: "hidden",
-        }}
-      ></div>
-      <div
-        className="absolute bg-color4"
-        style={{
-          width: "35%",
-          height: "45%",
-          left: "60%",
-          top: "50%",
-          filter: "blur(400px)",
-          transform: "rotate(-147.52deg)",
-          zIndex: "-1",
-          overflow: "hidden",
-        }}
-      ></div>
 
       {/* Sub-grid for Page 1 */}
       <div className="z-10 grid grid-cols-12 grid-rows-6 gap-1 overflow-auto column">
