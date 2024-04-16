f00818368865ce92195498db81932cc3ab154163
Masheen88 <mattdrummerguy@gmail.com>
2024-04-16 17:43:00 -0500

    Update CSS to disable horizontal scroll and adjust background blur in heroPage.jsx

--- src/components/pages/heroPage.jsx
+++ src/components/pages/heroPage.jsx
@@ -2,30 +2,37 @@
 import "./heroPage.css";
 
 function HeroPage() {
+  const backGroundBlurAmount = 400;
+  const backGroundBorderRadius = 50;
+
   return (
-    <div className="relative col-span-12 row-span-1 row-start-1 overflow-hidden">
+    <div className="relative col-span-12 row-start-2 overflow-hidden  md:col-span-4 md:col-start-2 lg:col-end-4">
       {/*NOTE Background Colors */}
+
       {/* rose */}
       <div
-        className="absolute bg-color1"
+        className="absolute col-start-1 row-start-6 bg-color1"
         style={{
           width: "50%",
           height: "50%",
-          left: "0%",
-          top: "50%",
-          filter: "blur(200px)",
+          left: "-10%",
+          top: "60%",
+          filter: `blur(${backGroundBlurAmount}px)`,
+          borderRadius: `${backGroundBorderRadius}%`,
           zIndex: "-1",
         }}
       ></div>
+
       {/* lime */}
       <div
-        className="absolute bg-color2"
+        className="absolute col-start-2 row-start-3 bg-color2"
         style={{
           width: "40%",
           height: "60%",
-          left: "20%",
-          top: "15%",
-          filter: "blur(200px)",
+          left: "25%",
+          top: "25%",
+          filter: `blur(${backGroundBlurAmount}px)`,
+          borderRadius: `${backGroundBorderRadius}%`,
           zIndex: "-1",
         }}
       ></div>
@@ -35,9 +42,10 @@ function HeroPage() {
         style={{
           width: "30%",
           height: "40%",
-          left: "62%",
-          top: "5%",
-          filter: "blur(200px)",
+          left: "65%",
+          top: "-15%",
+          filter: `blur(${backGroundBlurAmount}px)`,
+          borderRadius: `${backGroundBorderRadius}%`,
           zIndex: "-1",
         }}
       ></div>
@@ -49,8 +57,8 @@ function HeroPage() {
           height: "45%",
           left: "72%",
           top: "50%",
-          filter: "blur(400px)",
-          transform: "rotate(-147.52deg)",
+          filter: `blur(${backGroundBlurAmount}px)`,
+          borderRadius: `${backGroundBorderRadius}%`,
           zIndex: "-1",
         }}
       ></div>
@@ -60,13 +68,13 @@ function HeroPage() {
         {/* Add your rows and columns inside the sub-grid */}
 
         <div className="col-span-4 col-start-2 col-end-4 row-start-2">
-          <h2 className="text-6xl font-bold leading-tight tracking-tighter text-left">
+          <h2 className="text-4xl font-bold leading-tight tracking-tighter md:text-6xl">
             Developing Responsive Websites For Your Brand
           </h2>
         </div>
         <div className="content-center col-span-3 col-start-2 row-span-1 row-start-3 mt-14">
           <button
-            className="text-6xl leading-none tracking-tight text-center duration-500 bg-yellow-600 hover:bg-yellow-500"
+            className="text-5xl leading-none tracking-tight text-center duration-700 bg-yellow-600 md:text-2x1 hover:bg-yellow-500"
             style={{
               position: "absolute",
               width: "350px",
