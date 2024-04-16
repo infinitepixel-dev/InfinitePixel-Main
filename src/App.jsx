1893d996c6d762f391112b6d66ee44f0a4377477
Masheen88 <mattdrummerguy@gmail.com>
2024-04-16 17:08:20 -0500

    Added grid / page components to clean up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -38,137 +38,24 @@ function App() {
         <>
           {/*NOTE Navigation Bar - Displays the Navigation Bar outside of grid container */}
           <DynamicComponentLoader componentName="NavigationBar" />
-
           {/*NOTE Main Content Grid Container - BEGIN */}
           {/* <div className="grid w-screen h-screen grid-cols-12 grid-rows-4 gap-1"> */}
           {/* Page 1 */}
-          <div className="relative col-span-12 row-span-1 row-start-1">
-            {/* <div id="color1"></div>
-              <div id="color2"></div>
-              <div id="color3"></div>
-              <div id="color4"></div> */}
-
-            <div
-              className="absolute bg-color1"
-              style={{
-                width: "50%",
-                height: "50%",
-                left: "0%",
-                top: "50%",
-                filter: "blur(200px)",
-                zIndex: "-1",
-              }}
-            ></div>
-            <div
-              className="absolute bg-color2"
-              style={{
-                width: "40%",
-                height: "60%",
-                left: "20%",
-                top: "15%",
-                filter: "blur(200px)",
-                zIndex: "-1",
-              }}
-            ></div>
-            <div
-              className="absolute bg-color3"
-              style={{
-                width: "30%",
-                height: "40%",
-                left: "50%",
-                top: "5%",
-                filter: "blur(200px)",
-                zIndex: "-1",
-              }}
-            ></div>
-            <div
-              className="absolute bg-color4"
-              style={{
-                width: "35%",
-                height: "45%",
-                left: "60%",
-                top: "50%",
-                filter: "blur(400px)",
-                transform: "rotate(-147.52deg)",
-                zIndex: "-1",
-              }}
-            ></div>
-
-            {/* Sub-grid for Page 1 */}
-            <div className="z-10 grid grid-cols-12 grid-rows-6 gap-1 overflow-auto column">
-              {/* Add your rows and columns inside the sub-grid */}
-
-              <div className="col-span-5 col-start-2 row-start-2">
-                <h2 className="text-6xl leading-tight tracking-tighter text-left">
-                  Developing Responsive Websites For Your Brand
-                </h2>
-              </div>
-              <div className="content-center col-span-3 col-start-2 row-span-1 row-start-3 mt-14">
-                <button
-                  className="text-6xl leading-none tracking-tight text-center duration-500 bg-yellow-600 hover:bg-yellow-500"
-                  style={{
-                    position: "absolute",
-                    width: "350px",
-                    height: "89px",
-                    // left: "148px",
-                    // top: "485px",
-                    color: "black",
-                    borderRadius: "15px",
-                  }}
-                >
-                  Get Started
-                </button>
-              </div>
-              <div className="col-span-2 row-span-2">Content 3</div>
-              <div className="col-span-4 row-span-2">Content 4</div>
-            </div>
-          </div>
-
+          <DynamicComponentLoader componentName="HeroPage" />
           {/* Page 2 */}
-          <div className="relative col-span-12 row-span-1 row-start-2">
-            {/* Sub-grid for Page 2 */}
-            <div className="z-10 grid w-screen h-screen grid-cols-6 grid-rows-4 gap-1 overflow-auto column">
-              {/* Define your sub-grid structure here */}
-              <div className="col-span-3 row-span-1">Content 1</div>
-              <div className="col-span-3 row-span-1">Content 2</div>
-              <div className="col-span-2 row-span-2">Content 3</div>
-              <div className="col-span-4 row-span-2">Content 4</div>
-            </div>
-          </div>
+          <DynamicComponentLoader componentName="Page2" />
 
           {/* Page 3 */}
-          <div className="relative col-span-12 row-span-1 row-start-3">
-            {/* Sub-grid for Page 3 */}
-            <div className="z-10 grid w-screen h-screen grid-cols-6 grid-rows-4 gap-1 overflow-auto column">
-              {/* Define your sub-grid structure here */}
-              <div className="col-span-3 row-span-1">Content 1</div>
-              <div className="col-span-3 row-span-1">Content 2</div>
-              <div className="col-span-2 row-span-2">Content 3</div>
-              <div className="col-span-4 row-span-2">Content 4</div>
-            </div>
-          </div>
+          <DynamicComponentLoader componentName="Page3" />
 
           {/* Page 4 */}
-          <div className="relative col-span-12 row-span-1 row-start-4">
-            {/* Sub-grid for Page 4 */}
-            <div className="z-10 grid w-screen h-screen grid-cols-6 grid-rows-4 gap-1 overflow-auto column">
-              {/* Define your sub-grid structure here */}
-              <div className="col-span-3 row-span-1">Content 1</div>
-              <div className="col-span-3 row-span-1">Content 2</div>
-              <div className="col-span-2 row-span-2">Content 3</div>
-              <div className="col-span-4 row-span-2">Content 4</div>
-            </div>
-          </div>
+          <DynamicComponentLoader componentName="Page4" />
+
           {/* </div> */}
-
           {/*NOTE - Background Colors */}
-
           {/*NOTE - Hero Text */}
-
           {/*NOTE - Hero CTA */}
-
           {/* NOTE - Information */}
-
           {/* Row 1 */}
           {/* <div className="col-span-6 row-span-1 m-10 text-center column">
        
@@ -188,7 +75,6 @@ function App() {
                 </div>
               </div>
             </div> */}
-
           {/*NOTE Best Component Ever!!!!! - Takes remaining space on medium screens and one-third on large screens */}
           {/* <div className="col-span-2 col-start-1 column">
               <DynamicComponentLoader
@@ -197,14 +83,12 @@ function App() {
                 prop1="blahblahblah"
               />
             </div> */}
-
           {/*NOTE Component B and Pixel Counter - Full Row on small screens, full width on medium and two-thirds on large screens */}
           {/* <DynamicComponentLoader
               className="column"
               componentName="ComponentB"
               prop1="value1"
             /> */}
-
           {/*NOTE Pixel Counter Testing */}
           {/* <div className="content-center column md:col-span-2">
               <div className="grid row-span-3 text-center grid-rows-subgrid gap4">
@@ -213,7 +97,6 @@ function App() {
                 </h3>
               </div>
             </div> */}
-
           {/* Main Content Grid Container - END */}
         </>
         //!SECTION Main Content Container - BEGIN
---
+++ src/components/pages/page2.jsx
@@ -0,0 +1,90 @@
+// Hero - Page 1
+import "./heroPage.css";
+
+function HeroPage() {
+  return (
+    <div className="relative col-span-12 row-span-1 row-start-1 overflow-hidden">
+      {/*NOTE Background Colors */}
+      <div
+        className="absolute bg-color1"
+        style={{
+          width: "50%",
+          height: "50%",
+          left: "0%",
+          top: "50%",
+          filter: "blur(200px)",
+          zIndex: "-1",
+          overflow: "hidden",
+        }}
+      ></div>
+      <div
+        className="absolute bg-color2"
+        style={{
+          width: "40%",
+          height: "60%",
+          left: "20%",
+          top: "15%",
+          filter: "blur(200px)",
+          zIndex: "-1",
+          overflow: "hidden",
+        }}
+      ></div>
+      <div
+        className="absolute bg-color3"
+        style={{
+          width: "30%",
+          height: "40%",
+          left: "50%",
+          top: "5%",
+          filter: "blur(200px)",
+          zIndex: "-1",
+          overflow: "hidden",
+        }}
+      ></div>
+      <div
+        className="absolute bg-color4"
+        style={{
+          width: "35%",
+          height: "45%",
+          left: "60%",
+          top: "50%",
+          filter: "blur(400px)",
+          transform: "rotate(-147.52deg)",
+          zIndex: "-1",
+          overflow: "hidden",
+        }}
+      ></div>
+
+      {/* Sub-grid for Page 1 */}
+      <div className="z-10 grid grid-cols-12 grid-rows-6 gap-1 overflow-auto column">
+        {/* Add your rows and columns inside the sub-grid */}
+
+        <div className="col-span-5 col-start-2 row-start-2">
+          <h2 className="text-6xl font-bold leading-tight tracking-tighter text-left">
+            Page 2
+          </h2>
+        </div>
+        <div className="content-center col-span-3 col-start-2 row-span-1 row-start-3 mt-14">
+          <button
+            className="text-6xl leading-none tracking-tight text-center duration-500 bg-yellow-600 hover:bg-yellow-500"
+            style={{
+              position: "absolute",
+              width: "350px",
+              height: "89px",
+              // left: "148px",
+              // top: "485px",
+              color: "black",
+              borderRadius: "15px",
+            }}
+          >
+            Get Started
+          </button>
+        </div>
+        <div className="col-span-2 row-span-2">Content 3</div>
+        <div className="col-span-4 row-span-2">Content 4</div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroPage;
