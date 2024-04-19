dd016bb20d8766869f8113bc7166b2ab1c93259d
ShreddShark <daniel.cox2685@gmail.com>
2024-04-19 11:01:50 -0500

    added bg image and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      opacity: {
+        2: ".02",
+      },
       keyframes: {
         typing: {
           "0%": {
@@ -43,4 +46,4 @@ export default {
     },
   },
   plugins: [],
-};
+}
