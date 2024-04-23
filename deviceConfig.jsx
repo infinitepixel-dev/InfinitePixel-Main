c0180660a9179304eba8e1dbf2ad1e643c032e7c
Masheen88 <mattdrummerguy@gmail.com>
2024-04-22 20:37:48 -0500

    Add deviceConfig.jsx with stylesConfig object

---
+++ deviceConfig.jsx
@@ -0,0 +1,21 @@
+export const stylesConfig = {
+  d1440: {
+    heroPage: {
+      heroPageElement:
+        "grid w-full h-screen grid-cols-8 overflow-hidden grid-rows-8",
+      heroTitle:
+        "col-span-7 col-start-2 col-end-7 text-5xl font-bold leading-normal tracking-normal text-8xl",
+      getStartedButton:
+        "self-center col-start-2 col-end-8 row-start-4 text-4xl leading-normal tracking-tight text-center duration-700 rounded-lg text-5xl",
+      servicesGrid: "grid grid-cols-1 col-span-6 col-start-2 row-start-5 mt-6",
+      serviceItem: "m-3 mt-2 text-xl font-bold leading-normal tracking-normal",
+      serviceDescription: "mt-2 text-lg leading-7",
+    },
+    navigationBar: {
+      navbarElement: "grid w-full h-16 grid-cols-8 overflow-hidden",
+      navbarLogo: "col-start-2 col-end-3 self-center",
+      navbarMenu: "col-start-4 col-end-7 row-start-1 self-center",
+      navbarMenuItem: "ml-4 text-lg font-bold leading-7 tracking-normal",
+    },
+  },
+};
