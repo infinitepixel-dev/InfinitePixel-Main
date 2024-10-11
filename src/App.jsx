3e36c1413482023de7e164055111b5789a4505ff
ShreddShark <daniel.cox2685@gmail.com>
2024-10-10 23:02:43 -0500

    added new about section, and icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css"
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
-import CustomCursor from "./components/CustomCursor"
+import WhatWeDo from "./components/WhatWeDo"
+// import CustomCursor from "./components/CustomCursor"
 
 function App() {
   return (
-    <div className="min-h-screen bg-sky-950">
-      <CustomCursor />
+    <div className="min-h-screen overflow-hidden bg-sky-950">
+      {/* <CustomCursor /> */}
       <Navbar />
       <Header />
+      <WhatWeDo />
     </div>
   )
 }
