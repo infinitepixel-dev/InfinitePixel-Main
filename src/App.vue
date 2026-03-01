242f408373274a49dded99d4e6955c55ed6c3ea9
Daniel Cox <daniel.cox2685@gmail.com>
2026-03-01 10:36:32 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from './app/components/pages/Header.vue'
+import WhatWeDo from './app/components/pages/WhatWeDo.vue'
+import ProjectStats from './app/components/pages/ProjectStats.vue'
+// import WhyCustomWebsite from './app/components/pages/WhyCustomWebsite.vue'
+// import ScrollingIconsBar from './app/components/pages/ScrollingIconsBar.vue'
+import AboutUs from './app/components/pages/AboutUs.vue'
+import ContactForm from './app/components/pages/ContactForm.vue'
+// import Map from './app/components/pages/Map.vue'
+</script>
+
+<template>
+  <link href="/src/style.css" rel="stylesheet" />
+
+  <Header />
+  <WhatWeDo />
+  <ProjectStats />
+  <WhyCustomWebsite />
+  <ScrollingIconsBar />
+  <AboutUs />
+  <ContactForm />
+  <Map />
+</template>
